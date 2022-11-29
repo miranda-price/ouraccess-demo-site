97ad002f0bf98e7db0bd427873e5a2014fa8a740
Miranda Price <miranda27price@gmail.com>
2022-11-29 17:37:12 -0500

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoutingComponent } from './routing/routing.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { PathsComponent } from './paths/paths.component';
+import { DirectionsComponent } from './directions/directions.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponent,
+    UserInputComponent,
+    PathsComponent,
+    DirectionsComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
